c94269409315bf827a5bee409b155fe0ad46c519
honki <honkkki@yahoo.com>
2022-12-16 18:59:31 +0800

    update

---
+++ code/mall/service/product/rpc/internal/logic/detaillogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"github.com/honkkki/gomall/code/mall/service/product/rpc/internal/svc"
+	"github.com/honkkki/gomall/code/mall/service/product/rpc/types/product"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type DetailLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewDetailLogic(ctx context.Context, svcCtx *svc.ServiceContext) *DetailLogic {
+	return &DetailLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *DetailLogic) Detail(in *product.DetailRequest) (*product.DetailResponse, error) {
+	// todo: add your logic here and delete this line
+
+	return &product.DetailResponse{}, nil
+}
---
+++ code/mall/service/product/rpc/internal/logic/updatelogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"github.com/honkkki/gomall/code/mall/service/product/rpc/internal/svc"
+	"github.com/honkkki/gomall/code/mall/service/product/rpc/types/product"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UpdateLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewUpdateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UpdateLogic {
+	return &UpdateLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *UpdateLogic) Update(in *product.UpdateRequest) (*product.UpdateResponse, error) {
+	// todo: add your logic here and delete this line
+
+	return &product.UpdateResponse{}, nil
+}
